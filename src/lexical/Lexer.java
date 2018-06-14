621a08707ecc8d0db5cb2eed088016c0472d6a2e
jack <jjy_8888@163.com>
2018-06-15 07:36:28 +0800

    refactor lexer code

--- src/lexical/Lexer.java
+++ src/lexical/Lexer.java
@@ -12,10 +12,12 @@ public class Lexer implements Closeable {
 
 	private FileScanner fileScanner;
 	private int ch;
-
+	private StringBuilder content;
+	
 	public Lexer(String filePath) throws IOException {
 		this.fileScanner = new FileScanner(filePath);
 		this.ch = ' ';
+		this.content = new StringBuilder();
 	}
 
 	/**
@@ -25,56 +27,18 @@ public class Lexer implements Closeable {
 	 * 
 	 */
 	public Token getNextToken() {
-		StringBuilder content = new StringBuilder();
 		Token t = null;
-		
+
 		while (ch != -1) {
 			// 忽略空白字符
 			while (Character.isWhitespace(ch)) {
 				this.scan();
 			}
 
-			if (Character.isLetterOrDigit(ch) || ch == '_') {
-				// 标识符，关键字
-				content.setLength(0);
-				do {
-					content.append(ch);
-					scan();
-				} while (Character.isLetterOrDigit(ch) || ch == '_');
-				String id = content.toString();
-				TagEnum tag = KeywordMaps.getTag(id);
-				if (tag == TagEnum.ID) {
-					t = new Id(id);
-				} else {
-					t = new Token(tag);
-				}
-			}
-			else if (ch == '"'){
-				//字符串
-				content.setLength(0);
-				while(!scan('"')){
-					if (ch == '\\'){ //转义
-						scan();
-						if (ch == 'n'){
-							content.append(ch);
-						}else if (ch == '\\'){
-							content.append('\\');
-						}else if (ch == 't'){
-							content.append('\t');
-						}else if (ch == '"'){
-							content.append('"');
-						}else if (ch == '0'){
-							content.append('\0');
-						}else if (ch == '\n'){
-							
-						}else if (ch == -1){
-							//LEXERROR(STR_NO_R_QUTION);
-							t=new Token(TagEnum.ERR);
-							break;
-						}
-						
-					}
-				}
+			if (isIdOrKeyword()) {
+				t = recognizeIdOrKeyword();
+			} else if (isStr()) {
+				t = recognizeStr();
 			}
 		}
 
@@ -93,6 +57,80 @@ public class Lexer implements Closeable {
 	private boolean scan() {
 		ch = this.fileScanner.getNextChar();
 		return true;
+	}
+	
+	/**
+	 * 是否标识行，关键字 ，以字母或下划线开始
+	 * */
+	private boolean isIdOrKeyword(){
+		return Character.isLetter(ch) || ch == '_';
+	}
+	
+	/**
+	 * 标识符，关键字识别， 非字母，数字或下划线结束
+	 * */
+	private Token recognizeIdOrKeyword(){
+		content.setLength(0);
+		do {
+			content.append(ch);
+			scan();
+		} while (Character.isLetterOrDigit(ch) || ch == '_');
+		String id = content.toString();
+		TagEnum tag = KeywordMaps.getTag(id);
+		if (tag == TagEnum.ID) {
+			return new Id(id);
+		} else {
+			return new Token(tag);
+		}
+	}
+	
+	/**
+	 * 是否字符串 以双引号"开始
+	 * */
+	private boolean isStr(){
+		return ch == '"';
+	}
+	
+	/**
+	 * 识别字符串  支持\\, \n, \t, \", \0转义，中间可以是任何其它符， 遇到双引号"结束
+	 * */
+	private Token recognizeStr(){
+		Token t = null;
+		content.setLength(0);
+		while (!scan('"')) {
+			if (ch == '\\') { // 转义处理
+				scan();
+				if (ch == 'n') {
+					content.append(ch);
+				} else if (ch == '\\') {
+					content.append('\\');
+				} else if (ch == 't') {
+					content.append('\t');
+				} else if (ch == '"') {
+					content.append('"');
+				} else if (ch == '0') {
+					content.append('\0');
+				} else if (ch == '\n') {
+
+				} else if (ch == -1) {
+					// LEXERROR(STR_NO_R_QUTION);
+					t = new Token(TagEnum.ERR);
+					break;
+				} else {
+					content.append(ch);
+				}
+			} else if (ch == -1) {
+				// LEXERROR(STR_NO_R_QUTION);
+				t = new Token(TagEnum.ERR);
+				break;
+			} else {
+				content.append(ch);
+			}
+		}
+		if (t == null) {
+			t = new Str(content.toString());
+		}
+		return t;
 	}
 
 	@Override
