d51e5ef0433260b3cfdcc19ab98e8adc33682191
Jack <jjy_8888@163.com>
2019-05-29 17:53:08 +0800

    Update Expr.java

--- src/main/java/com/toyc/semantic/Expr.java
+++ src/main/java/com/toyc/semantic/Expr.java
@@ -15,14 +15,17 @@ public abstract class Expr extends AbstractRuleNode {
     }
 
     /**
-     * 生成中间代码
+     * 返回一个结果，该结果可以成为三地址指令的右部
+     * E = E1 + E2  方法gen返回x1 + x2, x1和x2分别存放E1和E2值的地址
      */
     public Expr gen() {
         return this;
     }
 
     /**
-     * 生成化简中间代码
+     * 表达式归约为单一地址
+     * 返回一个常量，一个标识符，或都一个临时的名称
+     * 给定一个表达式E, 返回一个存放E的值临时变量
      */
     public Expr reduce() {
         return this;
@@ -31,9 +34,7 @@ public abstract class Expr extends AbstractRuleNode {
     /**
      * 根据true或false label生成跳转指令
      */
-    public void jumping(int tLabel, int fLabel) {
-        this.emitJumps(this.arg, tLabel, fLabel);
-    }
+    public abstract void jumping(int tLabel, int fLabel);
 
     /**
      * t和f的label数值和test生成跳转指令
